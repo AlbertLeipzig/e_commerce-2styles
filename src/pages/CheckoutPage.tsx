70533e16e2097a26f61b1f4624b0676a22e41a1d
AlbertLeipzig <albertclemvill@gmail.com>
2023-11-14 22:43:48 +0100

    finished

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,5 +1,75 @@
+const cartData = [
+  {
+    img: '',
+    title: 'H1 Gamepad',
+    price: 650,
+  },
+  {
+    img: '',
+    title: 'LCD Monitor',
+    price: 1100,
+  },
+];
+
+const calculateCosts = () => {
+  const subtotal = cartData.reduce((acc, curr) => acc + curr.price, 0);
+  const shipping = subtotal > 500 ? 0 : subtotal * 0.2; // Corrected the shipping calculation
+  const total = subtotal + shipping;
+
+  return { subtotal, shipping, total };
+};
+
 export const CheckoutPage = () => {
-    return(
-        <h1>Checkout Page</h1>
-    )
-}
+  return (
+    <div className="checkout-page">
+      <h2>Billing Details</h2>
+      <form action=""></form>
+      <div className="cart-container">
+        <div className="cart-container__products">
+          {cartData.map((product, index) => (
+            <div key={index}>
+              <img src={product.img} alt={`${product.title} image`} />
+              <h3>{product.title}</h3>
+              <p>{product.price} $</p>
+            </div>
+          ))}
+        </div>
+        <div className="cart-container__invoice">
+          <div>
+            <p>Subtotal: {calculateCosts().subtotal} $</p>{' '}
+            <p>
+              Shipping:{' '}
+              {calculateCosts().shipping === 0
+                ? 'Free'
+                : `${calculateCosts().shipping} $`}
+            </p>
+            <p>Total: {calculateCosts().total} $</p>
+          </div>
+        </div>
+        <div className="cart-container__payment-system">
+          <div>
+            <label htmlFor="">
+              Bank
+              <input type="checkbox" />
+            </label>
+            <div>
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
+          </div>
+          <label htmlFor="">
+            Cash on delivery
+            <input type="checkbox" />
+          </label>
+          <div>
+            <input type="text" placeholder="Coupon Code" />
+            <button>Apply Coupon</button>
+          </div>
+        </div>
+        <button>Place Order</button>
+      </div>
+    </div>
+  );
+};
