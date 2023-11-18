7b0055f2b3e7be2272dca9ce64c6b254e7ff1b80
AlbertLeipzig <albertclemvill@gmail.com>
2023-11-18 23:14:56 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 export const App = () => {
   return (
-    <>
+    <div className='app'>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 };
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -5,8 +5,11 @@ export const AuthPage = () => {
 
   return (
     <div className="auth-page">
-      <img src="" alt="" />
-      <form action="">
+      <img
+        src="https://images.unsplash.com/photo-1511556820780-d912e42b4980?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHN8ZW58MHx8MHx8fDA%3D"
+        alt=""
+      />
+      <div>
         <h1>
           {authView === 'login' ? 'Log in to Exclusive' : 'Create an account'}
         </h1>
@@ -16,25 +19,23 @@ export const AuthPage = () => {
 
           <input type="text" placeholder="Email or Phone Number" />
           <input type="text" placeholder="Password" />
-          <div>
-            <button onClick={() => console.log('pendant to program')}>
-              {authView === 'login' ? 'Log In' : 'Create Account'}
-            </button>
-            {authView === 'login' && (
-              <NavLink to="password-recover">Forget Password</NavLink>
-            )}
-            {authView === 'signin' && (
-              <NavLink to="password-recover">Sign Up With Google</NavLink>
-            )}
-            {authView === 'signin' && (
-              <p>
-                Already have account?{' '}
-                <span onClick={() => setAuthView('login')}>Log in</span>
-              </p>
-            )}
-          </div>
+          <button onClick={() => console.log('pendant to program')}>
+            {authView === 'login' ? 'Log In' : 'Create Account'}
+          </button>
+          {authView === 'login' && (
+            <NavLink to="password-recover" >Forget Password</NavLink>
+          )}
+          {authView === 'signin' && (
+            <NavLink to="password-recover" className="google-signup">Sign Up With Google</NavLink>
+          )}
+          {authView === 'signin' && (
+            <p>
+              Already have account?{' '}
+              <span onClick={() => setAuthView('login')}>Log in</span>
+            </p>
+          )}
         </form>
-      </form>
+      </div>
     </div>
   );
 };
