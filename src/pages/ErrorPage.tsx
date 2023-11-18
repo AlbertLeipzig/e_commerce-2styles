3e20355b2bd9d9ce37277424a73dbf1957733971
AlbertLeipzig <albertclemvill@gmail.com>
2023-11-18 23:25:28 +0100

    wip

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,11 +4,17 @@ export const ErrorPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="error_page">
-      <h1>404 Not Found</h1>
-      <p>Your visited page not found. You may go home page.</p>º{' '}
-      <NavLink to="/">Go To Home Page</NavLink>
-      <button onClick={() => navigate(-1)}>Go Back to Previous Page</button>
+    <div className="error-page">
+      <div className="error-page__content">
+        <h1>404 Not Found</h1>
+        <p>Your visited page not found. You may go home page.</p>º{' '}
+        <div className="error-page__buttons">
+          <button>
+            <NavLink to="/">Go To Home Page</NavLink>
+          </button>
+          <button onClick={() => navigate(-1)}>Go Back to Previous Page</button>
+        </div>
+      </div>
     </div>
   );
 };
